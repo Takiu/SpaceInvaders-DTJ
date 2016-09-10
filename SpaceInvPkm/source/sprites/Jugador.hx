5b07ab28bcd5d024aea77c2f9cfb90c4239b741b
Julian Volonterio <julianvolonterio@gmail.com>
2016-09-09 21:32:36 -0300

    testjuli

---
+++ SpaceInvPkm/source/sprites/Jugador.hx
@@ -0,0 +1,25 @@
+package sprites;
+
+import flixel.FlxSprite;
+import flixel.FlxButton;
+import sprites.spr;
+
+class Jugador extends Nave
+{
+
+	var vida: Int;
+	var highscore : Int;
+	
+	public function new()
+	{
+	spr = new FlxSprite(100, 200);
+	spr.makeGraphic(64, 64, 0xFFFF00);
+	add(spr);
+	}
+	
+	override public function Mover() 
+	{
+		if(FlxG.keys.p
+	}
+	
+}
