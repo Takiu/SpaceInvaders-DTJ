fd399745a018c8d528de62def7b664adcd903fc3
Taku <takunori.kasai@gmail.com>
2016-09-16 22:05:19 -0300

    tertsd

--- SpaceInvPkm/source/states/Reiniciar.hx
+++ SpaceInvPkm/source/states/Reiniciar.hx
@@ -18,23 +18,30 @@ class Reiniciar extends FlxState
 		super.create();	
 		if (Reg.Fin) {
 			//Gano
-			texto = "GANASTE";		
+			texto = "¡GANASTE!";		
 			color = 0x00FF40;
 		}
 		else {
 			//Perdio
-			texto = "PERDISTE";		
+			texto = "¡PERDISTE!";		
 			color = 0xffff0000;
 		}		
-		titulo = new FlxText(40, 50, 90, texto, 8, false);
+		titulo = new FlxText(0, 144, 150, texto, 20, false);
 		titulo.alignment = "center";
 		titulo.color = color;
-		add(titulo);
-		FlxG.switchState(new MenuState());
+		add(titulo);		
 	}
-	
+	var time : Int=0;
 	override public function update(elapsed:Float):Void
 	{		
+		time++;
+		if(time == 3){
+			titulo.y--;
+			time = 0;
+		}
+		if (titulo.y == 50) {
+			FlxG.switchState(new MenuState());
+		}
 		super.update(elapsed);
 	}
 }
